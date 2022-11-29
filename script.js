87c82816e98d619e44b4fa696dad4a6714642322
Gregorh98 <33038097+Gregorh98@users.noreply.github.com>
2022-11-29 16:28:02 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -13,25 +13,25 @@ function calculateStoryPoints()
 	switch(sliderVal)
 	{
 		case 0:
-			document.getElementById("estimatedStoryPoints").innerHTML = 0
+			document.getElementById("estimatedStoryPoints").innerHTML = '<h1 class="display-1">1</h1>'
 			break;
 		case 1:
-			document.getElementById("estimatedStoryPoints").innerHTML = 1
+			document.getElementById("estimatedStoryPoints").innerHTML = '<h1 class="display-1">2</h1>'
 			break;
 		case 2:
-			document.getElementById("estimatedStoryPoints").innerHTML = 2
+			document.getElementById("estimatedStoryPoints").innerHTML = '<h1 class="display-1">3</h1>'
 			break;
 		case 3:
-			document.getElementById("estimatedStoryPoints").innerHTML = 3
+			document.getElementById("estimatedStoryPoints").innerHTML = '<h1 class="display-1">5</h1>'
 			break;
 		case 4:
-			document.getElementById("estimatedStoryPoints").innerHTML = 5
+			document.getElementById("estimatedStoryPoints").innerHTML = '<h1 class="display-1">8</h1>'
 			break;
 		case 5:
-			document.getElementById("estimatedStoryPoints").innerHTML = 8
+			document.getElementById("estimatedStoryPoints").innerHTML = '<h1 class="display-1">13</h1>'
 			break;
 		case 6:
-			document.getElementById("estimatedStoryPoints").innerHTML = 13
+			document.getElementById("estimatedStoryPoints").innerHTML = '<h1 class="display-1">13</h1>'
 			break;
 	}
 }
@@ -41,13 +41,13 @@ function updateMessages(u, c, e)
 	switch(u)
 	{
 		case 0:
-			document.getElementById("uncertaintyMessage").innerHTML = "I know exactly what is required to complete this work";
+			document.getElementById("uncertaintyMessage").innerHTML = "I know exactly what is required to complete this work and can visualise the change required.";
 			break;
 		case 1:
-			document.getElementById("uncertaintyMessage").innerHTML = "I have some outstanding gaps in my knowlege";
+			document.getElementById("uncertaintyMessage").innerHTML = "I have some outstanding gaps in my knowlege but know where to start and generally what is required.";
 			break;
 		case 2:
-			document.getElementById("uncertaintyMessage").innerHTML = "I have a lot of outstanding gaps in my knowlege";
+			document.getElementById("uncertaintyMessage").innerHTML = "I have a lot of outstanding gaps in my knowlege or am not confident about scope of the change.";
 			break;
 			
 	}
@@ -58,24 +58,31 @@ function updateMessages(u, c, e)
 			document.getElementById("complexityMessage").innerHTML = "This work is not reliant on any other work, and does not require any additional unit tests etc";
 			break;
 		case 1:
-			document.getElementById("complexityMessage").innerHTML = "This work is a mid-sized change and may have an impact on the way the system currently functions";
+			document.getElementById("complexityMessage").innerHTML = "This work is lightly coupled to other work, and may have an impact on the way the system currently functions";
 			break;
 		case 2:
-			document.getElementById("complexityMessage").innerHTML = "This work is a sweeping change and has high potential to impact the current system";
+			document.getElementById("complexityMessage").innerHTML = "This work is a heavily coupled to other work, sweeping change or has high potential to impact the current system";
 			break;			
 	}
 	
 	switch(e)
 	{
 		case 0:
-			document.getElementById("effortMessage").innerHTML = "This work is a minor change";
+			document.getElementById("effortMessage").innerHTML = "This work is a minor change, requiring making a change to one or two files";
 			break;
 		case 1:
-			document.getElementById("effortMessage").innerHTML = "This work is a mid-sized change";
+			document.getElementById("effortMessage").innerHTML = "This work is a mid-sized change, requiring a change to more than one project or a number of files";
 			break;
 		case 2:
-			document.getElementById("effortMessage").innerHTML = "This work is a large change";
+			document.getElementById("effortMessage").innerHTML = "This work is a large change, requiring changing multiple components, projects or involving a lot of branches and merges";
 			break;
 			
 	}
+}
+
+function labelClicked(n)
+{
+	var i = document.getElementById(n).innerHTML;
+	console.log(i);
+	document.getElementById(n).outerHTML = '<s>'+i+"</s>"
 }
